a85d89d20a9e584283109b31a29a7123094d4824
EduardoUDG <eduardo.cdorado@alumnos.udg.mx>
2021-09-03 12:23:42 -0500

    main

---
+++ main.py
@@ -0,0 +1,21 @@
+from PySide2.QtWidgets import QApplication
+from mainwindow import MainWindow
+import sys
+
+# Los archvios en minusculas
+# Las clases en mayusculas
+# from = nombre archivo | path
+# import clase, funcion, etc.
+
+# Aplicación de Qt
+app = QApplication()
+
+# Se crea la ventana Qt
+window = MainWindow()
+
+# Se imprime la ventana Qt
+window.show()
+
+# Ejecuta nuestra app | ventana Qt
+sys.exit(app.exec_())
+
