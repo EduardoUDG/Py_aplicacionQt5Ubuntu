681d7833993d6710d88b399c3d8df863b9214b94
EduardoUbuntu <eduardo.cdorado@alumnos.udg.mx>
2021-11-10 10:29:03 -0600

    toDic

--- views/mainwindow.py
+++ views/mainwindow.py
@@ -37,6 +37,7 @@ class MainWindow(QMainWindow):
         self.ui.limpiar.clicked.connect( self.limpiar )
         self.ui.dibujar.clicked.connect( self.dibujar )
         self.scene = QGraphicsScene()
+        self.scene2 = QGraphicsScene()
         self.ui.graphicsView.setScene( self.scene )
 
 
@@ -214,6 +215,37 @@ class MainWindow(QMainWindow):
     def mostrar(self):
         self.ui.salida_plainTextEdit.clear()
         self.ui.salida_plainTextEdit.insertPlainText( str( self.acelerador.Dparticulas ) + "\n" )
+
+    # def convertToDic(self):
+    #     pen2 = QPen()
+    #     pen2.setWidth(2)
+
+    #     d = {}
+    #     anterior = [0,0,0]
+    #     it = 1
+
+    #     for particula in self.acelerador:
+    #         self.ui.salida_plainTextEdit.clear()
+    #         self.ui.salida_plainTextEdit.insertPlainText( str( self.acelerador.Dparticulas ) + "\n" )
+
+    #         # código lista de adyacencia
+    #         if anterior[0] == 0:
+    #             d[(particula.origenX, particula.origenY)] = ( str(particula.origenX), str(particula.origenY), str(particula.distancia) )
+    #         else:
+    #             d[(particula.origenX, particula.origenY)] = ( str(anterior[0]), str(anterior[1]), str(anterior[2]) )
+    #         if it == len( self.acelerador ):
+    #             d[(str(particula.origenX), str(particula.origenY))] = ( str(particula.origenX), str(particula.origenY), str(particula.distancia) )
+    #         anterior[0] = str( particula.origenX )
+    #         anterior[1] = str( particula.origenY )
+    #         anterior[2] = str( particula.distancia )
+    #         it += 1
+
+
+    #     for key , value in d.items():
+    #         m = key, value
+    #         self.ui.salida_plainTextEdit.insertPlainText( str(m) + "\n" )
+
+
 
 
     def clearData(self):
